4523f1b68567a625b7cc0e80275aeb3619743256
Hp <yahye2092@gmail.com>
2023-05-26 13:28:22 +0300

    test

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ import express from "express";
 import contectdb from "./datebase/db.js";
 import userrouter from "./router/userRouter.js";
 import messagerouter from "./router/messageRouter.js";
-import dotenv from "dotenv";
+import { Server } from 'socket.io';
+import http from "http";
 
+
+import dotenv from "dotenv";
 dotenv.config()
 contectdb()
 const app = express()
 app.use(express.json())
 app.use('/api/user',userrouter)
 app.use('/api/message',messagerouter)
+const server = http.createServer(app);
+
+const socketio = new Server(server);
+
+
+socketio.on("connection", (socket)=>{
+    console.log("connetetd");
+    console.log(socket.id, "has joined");
+    socket.on("signin", (id) => {
+      console.log(id);
+      clients[id] = socket;
+      console.log(clients);});
+      socket.on("message", (msg) => {
+        console.log(msg);
+        let targetId = msg.targetId;
+        if (clients[targetId]) clients[targetId].emit("message", msg);
+      });
+
+})
 
 
 
